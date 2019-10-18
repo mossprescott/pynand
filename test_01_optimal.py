3d823277f80fa4ab8510078e74f6ec8b974ae6ac
Dan Fithian <dan.fithian@tvisioninsights.com>
2019-10-18 12:04:02 -0400

    Try to revert test changes

--- test_01_optimal.py
+++ test_01_optimal.py
@@ -18,10 +18,10 @@ def test_and():
     assert gate_count(And) == 2
 
 def test_xor():
-    assert gate_count(Xor) <= 5
+    assert gate_count(Xor) == 5
 
 def test_mux():
-    assert gate_count(Mux) <= 8
+    assert gate_count(Mux) == -1 # < 8
 
 def test_dmux():
-    assert gate_count(DMux) <= 5
+    assert gate_count(DMux) == 5
