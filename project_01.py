1c5199123ed44d3a1581219690dd1c83568b704c
Moss Prescott <moss@theprescotts.com>
2019-10-20 10:47:17 -0400

    Finish project_01 components

--- project_01.py
+++ project_01.py
@@ -60,6 +60,39 @@ def mkDMux(inputs, outputs):
 DMux = Component(mkDMux)
 
 
+def mkDMux4Way(inputs, outputs):
+    # TODO: optimal?
+    in_ = inputs.in_
+    sel = inputs.sel
+    lo = DMux(in_=in_, sel=sel[0])
+    outputs.a = DMux(in_=lo.a, sel=sel[1]).a
+    outputs.b = DMux(in_=lo.b, sel=sel[1]).a
+    outputs.c = DMux(in_=lo.a, sel=sel[1]).b
+    outputs.d = DMux(in_=lo.b, sel=sel[1]).b
+    
+DMux4Way = Component(mkDMux4Way)
+
+
+def mkDMux8Way(inputs, outputs):
+    # TODO: optimal?
+    # TODO: implement bit slice syntax and use DMux4Way?
+    in_ = inputs.in_
+    sel = inputs.sel
+    lo = DMux(in_=in_, sel=sel[0])
+    lo0 = DMux(in_=lo.a, sel=sel[1])
+    lo1 = DMux(in_=lo.b, sel=sel[1])
+    outputs.a = DMux(in_=lo0.a, sel=sel[2]).a
+    outputs.b = DMux(in_=lo1.a, sel=sel[2]).a
+    outputs.c = DMux(in_=lo0.b, sel=sel[2]).a
+    outputs.d = DMux(in_=lo1.b, sel=sel[2]).a
+    outputs.e = DMux(in_=lo0.a, sel=sel[2]).b
+    outputs.f = DMux(in_=lo1.a, sel=sel[2]).b
+    outputs.g = DMux(in_=lo0.b, sel=sel[2]).b
+    outputs.h = DMux(in_=lo1.b, sel=sel[2]).b
+    
+DMux8Way = Component(mkDMux8Way)
+
+
 def mkNot16(inputs, outputs):
     in_ = inputs.in_
     for i in range(16):
@@ -73,12 +106,31 @@ def mkAnd16(inputs, outputs):
         outputs.out[i] = And(a=inputs.a[i], b=inputs.b[i]).out
 
 And16 = Component(mkAnd16)
-        
 
-# TODO:
-# DMux4Way
-# DMux8Way
-# And16
-# Mux16
-# Mux4Way16
-# Mux8Way16
+
+def mkMux16(inputs, outputs):
+    for i in range(16):
+        outputs.out[i] = Mux(a=inputs.a[i], b=inputs.b[i], sel=inputs.sel).out
+
+Mux16 = Component(mkMux16)
+
+
+def mkMux4Way16(inputs, outputs):
+    # decode only once for fewer gates?
+    ab = Mux16(a=inputs.a, b=inputs.b, sel=inputs.sel[0]).out
+    cd = Mux16(a=inputs.c, b=inputs.d, sel=inputs.sel[0]).out
+    outputs.out = Mux16(a=ab, b=cd, sel=inputs.sel[1]).out    
+
+Mux4Way16 = Component(mkMux4Way16)
+
+
+def mkMux8Way16(inputs, outputs):
+    ab = Mux16(a=inputs.a, b=inputs.b, sel=inputs.sel[0]).out
+    cd = Mux16(a=inputs.c, b=inputs.d, sel=inputs.sel[0]).out
+    ef = Mux16(a=inputs.e, b=inputs.f, sel=inputs.sel[0]).out
+    gh = Mux16(a=inputs.g, b=inputs.h, sel=inputs.sel[0]).out
+    abcd = Mux16(a=ab, b=cd, sel=inputs.sel[1]).out
+    efgh = Mux16(a=ef, b=gh, sel=inputs.sel[1]).out
+    outputs.out = Mux16(a=abcd, b=efgh, sel=inputs.sel[2]).out
+            
+Mux8Way16 = Component(mkMux8Way16)
--- test_01.py
+++ test_01.py
@@ -50,6 +50,61 @@ def test_dmux():
 
     dmux11 = eval(DMux, in_=1, sel=1)
     assert dmux11.a == 0 and dmux11.b == 1
+    
+def test_dmux4way():
+    for sel in range(4):
+        result = eval(DMux4Way, in_=0, sel=sel)
+        assert result.a == 0 and result.b == 0 and result.c == 0 and result.d == 0
+
+    result = eval(DMux4Way, in_=1, sel=0b00)
+    assert result.a == 1 and result.b == 0 and result.c == 0 and result.d == 0
+
+    result = eval(DMux4Way, in_=1, sel=0b01)
+    assert result.a == 0 and result.b == 1 and result.c == 0 and result.d == 0
+
+    result = eval(DMux4Way, in_=1, sel=0b10)
+    assert result.a == 0 and result.b == 0 and result.c == 1 and result.d == 0
+
+    result = eval(DMux4Way, in_=1, sel=0b11)
+    assert result.a == 0 and result.b == 0 and result.c == 0 and result.d == 1
+
+def test_dmux8way():
+    for sel in range(8):
+        result = eval(DMux8Way, in_=0, sel=sel)
+        assert (result.a == 0 and result.b == 0 and result.c == 0 and result.d == 0
+            and result.e == 0 and result.f == 0 and result.g == 0 and result.h == 0)
+
+    result = eval(DMux8Way, in_=1, sel=0b000)
+    assert (result.a == 1 and result.b == 0 and result.c == 0 and result.d == 0
+        and result.e == 0 and result.f == 0 and result.g == 0 and result.h == 0)
+
+    result = eval(DMux8Way, in_=1, sel=0b001)
+    assert (result.a == 0 and result.b == 1 and result.c == 0 and result.d == 0
+        and result.e == 0 and result.f == 0 and result.g == 0 and result.h == 0)
+
+    result = eval(DMux8Way, in_=1, sel=0b010)
+    assert (result.a == 0 and result.b == 0 and result.c == 1 and result.d == 0
+        and result.e == 0 and result.f == 0 and result.g == 0 and result.h == 0)
+
+    result = eval(DMux8Way, in_=1, sel=0b011)
+    assert (result.a == 0 and result.b == 0 and result.c == 0 and result.d == 1
+        and result.e == 0 and result.f == 0 and result.g == 0 and result.h == 0)
+
+    result = eval(DMux8Way, in_=1, sel=0b100)
+    assert (result.a == 0 and result.b == 0 and result.c == 0 and result.d == 0
+        and result.e == 1 and result.f == 0 and result.g == 0 and result.h ==0)
+
+    result = eval(DMux8Way, in_=1, sel=0b101)
+    assert (result.a == 0 and result.b == 0 and result.c == 0 and result.d == 0
+        and result.e == 0 and result.f == 1 and result.g == 0 and result.h == 0)
+        
+    result = eval(DMux8Way, in_=1, sel=0b110)
+    assert (result.a == 0 and result.b == 0 and result.c == 0 and result.d == 0
+        and result.e == 0 and result.f == 0 and result.g == 1 and result.h == 0)
+
+    result = eval(DMux8Way, in_=1, sel=0b111)
+    assert (result.a == 0 and result.b == 0 and result.c == 0 and result.d == 0
+        and result.e == 0 and result.f == 0 and result.g == 0 and result.h == 1)
 
 def test_not16():
     assert eval(Not16, in_=0b0000_0000_0000_0000).out == 0b1111_1111_1111_1111
@@ -66,11 +121,32 @@ def test_and16():
     assert eval(And16, a=0b0011_1100_1100_0011, b=0b0000_1111_1111_0000).out == 0b0000_1100_1100_0000
     assert eval(And16, a=0b0001_0010_0011_0100, b=0b1001_1000_0111_0110).out == 0b0001_0000_0011_0100
 
-
-# TODO: these require multi-bit inputs/outputs:
-# DMux4Way
-# DMux8Way
-# And16
-# Mux16
-# Mux4Way16
-# Mux8Way16
+def test_mux16():
+    assert eval(Mux16, a=0b0000_0000_0000_0000, b=0b0000_0000_0000_0000, sel=0).out == 0b0000_0000_0000_0000
+    assert eval(Mux16, a=0b0000_0000_0000_0000, b=0b0000_0000_0000_0000, sel=1).out == 0b0000_0000_0000_0000
+    assert eval(Mux16, a=0b0000_0000_0000_0000, b=0b0001_0010_0011_0100, sel=0).out == 0b0000_0000_0000_0000
+    assert eval(Mux16, a=0b0000_0000_0000_0000, b=0b0001_0010_0011_0100, sel=1).out == 0b0001_0010_0011_0100
+    assert eval(Mux16, a=0b1001_1000_0111_0110, b=0b0000_0000_0000_0000, sel=0).out == 0b1001_1000_0111_0110
+    assert eval(Mux16, a=0b1001_1000_0111_0110, b=0b0000_0000_0000_0000, sel=1).out == 0b0000_0000_0000_0000
+    assert eval(Mux16, a=0b1010_1010_1010_1010, b=0b0101_0101_0101_0101, sel=0).out == 0b1010_1010_1010_1010
+    assert eval(Mux16, a=0b1010_1010_1010_1010, b=0b0101_0101_0101_0101, sel=1).out == 0b0101_0101_0101_0101
+    
+def test_mux4way16():
+    for i in range(4):
+        assert eval(Mux4Way16, a=0, b=0, c=0, d=0, sel=i).out == 0
+    assert eval(Mux4Way16, a=11111, b=22222, c=33333, d=44444, sel=0b00).out == 11111
+    assert eval(Mux4Way16, a=11111, b=22222, c=33333, d=44444, sel=0b01).out == 22222
+    assert eval(Mux4Way16, a=11111, b=22222, c=33333, d=44444, sel=0b10).out == 33333
+    assert eval(Mux4Way16, a=11111, b=22222, c=33333, d=44444, sel=0b11).out == 44444
+    
+def test_mux8way16():
+    for i in range(4):
+        assert eval(Mux8Way16, a=0, b=0, c=0, d=0, e=0, f=0, g=0, h=0, sel=i).out == 0
+    assert eval(Mux8Way16, a=11111, b=22222, c=33333, d=44444, e=55555, f=12345, g=23456, h=34567, sel=0b000).out == 11111
+    assert eval(Mux8Way16, a=11111, b=22222, c=33333, d=44444, e=55555, f=12345, g=23456, h=34567, sel=0b001).out == 22222
+    assert eval(Mux8Way16, a=11111, b=22222, c=33333, d=44444, e=55555, f=12345, g=23456, h=34567, sel=0b010).out == 33333
+    assert eval(Mux8Way16, a=11111, b=22222, c=33333, d=44444, e=55555, f=12345, g=23456, h=34567, sel=0b011).out == 44444
+    assert eval(Mux8Way16, a=11111, b=22222, c=33333, d=44444, e=55555, f=12345, g=23456, h=34567, sel=0b100).out == 55555
+    assert eval(Mux8Way16, a=11111, b=22222, c=33333, d=44444, e=55555, f=12345, g=23456, h=34567, sel=0b101).out == 12345
+    assert eval(Mux8Way16, a=11111, b=22222, c=33333, d=44444, e=55555, f=12345, g=23456, h=34567, sel=0b110).out == 23456
+    assert eval(Mux8Way16, a=11111, b=22222, c=33333, d=44444, e=55555, f=12345, g=23456, h=34567, sel=0b111).out == 34567
