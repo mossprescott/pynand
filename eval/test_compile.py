373e6209065748b74e63bcbfb404c8a815382730
Moss Prescott <moss@theprescotts.com>
2019-11-04 15:38:13 -0500

    Update for bit-slicing and the new constant zero bit

--- eval/test_compile.py
+++ eval/test_compile.py
@@ -24,8 +24,11 @@ def test_wrapper_nand():
     assert nand.out == 1
 
 
-def test_wrapper_swap():
-    """Test the wrapper that provides a nice syntax around a NandVector."""
+def test_wrapper_swap_bits():
+    """Test the wrapper that provides a nice syntax around a NandVector, this time
+    with a very simple case where two bits of one named input are mapped to the 
+    opposite bits of an output.
+    """
     
     swap_vec = NandVector(
         {('in_', 0): 0b01, ('in_', 1): 0b10},
@@ -52,12 +55,12 @@ def test_compile_nand():
     nand_vec = component_to_vector(Nand)  # TODO
 
     # Note: either input can be assigned to either of the low two bits.
-    assert set(nand_vec.inputs.keys()) == set(['a', 'b'])
-    assert set(nand_vec.inputs.values()) == set([0b001, 0b010])
+    assert set(nand_vec.inputs.keys()) == set([('a', None), ('b', None)])
+    assert set(nand_vec.inputs.values()) == set([0b0010, 0b0100])
 
-    assert nand_vec.outputs == {'out': 0b100}
+    assert nand_vec.outputs == {('out', None): 0b1000}
 
-    assert nand_vec.ops == [(0b011, 0b100)]
+    assert nand_vec.ops == [(0b0110, 0b1000)]
 
 
 def test_compile_not():
@@ -68,9 +71,9 @@ def test_compile_not():
 
     nand_vec = component_to_vector(Component(mkNott))
 
-    assert nand_vec.inputs == {('a', None): 0b01}
-    assert nand_vec.outputs == {('out', None): 0b10}
-    assert nand_vec.ops == [(0b01, 0b10)]
+    assert nand_vec.inputs == {('a', None): 0b010}
+    assert nand_vec.outputs == {('out', None): 0b100}
+    assert nand_vec.ops == [(0b010, 0b100)]
 
 
 def test_compile_multibit_nand():
@@ -83,9 +86,9 @@ def test_compile_multibit_nand():
 
     # Note: either input can be assigned to either of the low two bits.
     assert set(nand_vec.inputs.keys()) == set([('in_', 0), ('in_', 1)])
-    assert set(nand_vec.inputs.values()) == set([0b001, 0b010])
+    assert set(nand_vec.inputs.values()) == set([0b0010, 0b0100])
 
-    assert nand_vec.outputs == {('out', 2): 0b100}
+    assert nand_vec.outputs == {('out', 2): 0b1000}
 
-    assert nand_vec.ops == [(0b011, 0b100)]
+    assert nand_vec.ops == [(0b0110, 0b1000)]
 
