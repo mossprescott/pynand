615694c875fdebae5062b8cba63b810ffed84291
Moss Prescott <moss@theprescotts.com>
2019-11-06 20:47:59 -0500

    A working (but crude) DFF and Bit

--- project_03.py
+++ project_03.py
@@ -1,4 +1,4 @@
-from nand import Component
+from nand import Component, lazy
 from project_01 import *
 from project_02 import *
 
@@ -7,15 +7,19 @@ def mkDFF(inputs, outputs):
     # Note: provided as a primitive in the Nand to Tetris simulator, but implementing it
     # from scratch is fun.
 
+    in_ = inputs.in_
+    clock = inputs.clock  # TODO: make `clock` magic, so you don't have to propagate it everywhere?
+    
     def mkLatch(inputs, outputs):
-        d = lazy()
-        d.set(DMux(a=d.out, b=inputs.data, sel=inputs.store))
-        outputs.out = d.out
+        mux = lazy()
+        mux.set(Mux(a=mux.out, b=inputs.in_, sel=inputs.enable))
+        outputs.out = mux.out
     Latch = Component(mkLatch)
     
-    # TODO: make `clock` magic, so you don't have to propagate it everywhere?
-    inner = Latch(data=inputs.in_, store=And(a=inputs.store, b=Not(in_=inputs.clock).out).out).out
-    outputs.out = Latch(data=inner.out, store=inputs.clock).out
+    l1 = Latch(in_=inputs.in_, enable=inputs.clock)
+    l2 = Latch(in_=l1.out, enable=Not(in_=inputs.clock).out)
+    
+    outputs.out = l2.out
 
 DFF = Component(mkDFF)
 
@@ -23,13 +27,38 @@ DFF = Component(mkDFF)
 def mkBit(inputs, outputs):
     in_ = inputs.in_
     load = inputs.load
-    outputs.out = ___
+    clock = inputs.clock  # TODO: make `clock` magic, so you don't have to propagate it everywhere?
+    dff = lazy()
+    mux = Mux(a=dff.out, b=inputs.in_, sel=inputs.load)
+    dff.set(DFF(in_=mux.out, clock=clock))
+    outputs.out = dff.out
+
+Bit = Component(mkBit)
+
 
 def mkRegister(inputs, outputs):
-    pass
+    for i in range(16):
+        outputs.out[i] = Bit(in_=inputs.in_[i], load=inputs.load).out
+        
+Register = Component(mkRegister)
+
 
 def mkRAM8(inputs, outputs):
-    pass
+    load = DMux8Way(in_=inputs.load, sel=inputs.address)
+    reg0 = Register(in_=inputs.in_, load=load.a)
+    reg1 = Register(in_=inputs.in_, load=load.b)
+    reg2 = Register(in_=inputs.in_, load=load.c)
+    reg3 = Register(in_=inputs.in_, load=load.d)
+    reg4 = Register(in_=inputs.in_, load=load.e)
+    reg5 = Register(in_=inputs.in_, load=load.f)
+    reg6 = Register(in_=inputs.in_, load=load.g)
+    reg7 = Register(in_=inputs.in_, load=load.h)
+    outputs.out = Mux8Way16(a=reg0.out, b=reg1.out, c=reg2.out, d=reg3.out,
+                            e=reg4.out, f=reg5.out, g=reg6.out, h=reg7.out,
+                            sel=inputs.address)
+    
+RAM8 = Component(mkRAM8)
+
 
 def mkRAM64(inputs, outputs):
     pass
--- test_03.py
+++ test_03.py
@@ -5,33 +5,75 @@ def test_dff():
     
     assert dff.out == 0
     
-    dff.store = 1
-    dff.data = 1
-    # TODO: 
-    assert dff.out == 0  # Store not visible on the output yet
+    dff.in_ = 1
+    assert dff.out == 0
+    
+    dff.clock = 1
+    assert dff.out == 0
+    dff.clock = 0
+    assert dff.out == 1  # New value appears on the falling edge
 
-    dff.store = 0
-    dff.data = 0    
-    assert dff.out == 0  # Still not visible, and clearing data has no affect after store is lowered
+    dff.in_ = 0
+    assert dff.out == 1
     
     dff.clock = 1
-    assert dff.out == 1  # Now the output has the new value
+    assert dff.out == 1
+    dff.clock = 0
+    assert dff.out == 0  # New value appears on the falling edge
+    
+    # Now adjust the input while the clock is high:
+    dff.clock = 1
+    assert dff.out == 0
+    
+    dff.in_ = 1
+    assert dff.out == 0
     
     dff.clock = 0
-    assert dff.out == 1  # No change
+    assert dff.out == 0
+    
+    dff.clock = 1
+    assert dff.out == 1  # New value appears on the falling edge, still
 
-    dff.clock = 1
-    assert dff.out == 1  # Still no change
-    
     dff.clock = 0
-    assert dff.out == 1  # Still no change
+    assert dff.out == 1
     
-    dff.store = 1
-    dff.store = 0
-    assert dff.out == 1  # Update, but not exposed yet
+    # TODO: is this behavior good enough, or is it too sensitive to the order of applying signals?
+    # FIXME: minimally need a way to update the clock that forces values to propagate, so you don't 
+    # have to inspect values to get the correct behavior. Probably by making clock special (and 
+    # global), and providing tick() and tock() to cycle it.
     
-    dff.clock = 1
-    assert dff.out == 0  # Now you can see the new value
+
+def test_bit():
+    bit = run(Bit)
     
-    dff.clock = 0
-    assert dff.out == 0  # No change
+    assert bit.out == 0
+    
+    bit.load = 1
+    bit.in_ = 1
+    assert bit.out == 0  # New value not visible on the output yet
+
+    # bit.load = 0
+    # bit.in_ = 0
+    # assert bit.out == 0  # Still not visible, and clearing data has no affect after store is lowered
+    
+    bit.clock = 1
+    assert bit.out == 0  # No change
+    
+    bit.clock = 0
+    assert bit.out == 1  # Now you can see the new value
+
+    bit.clock = 1
+    assert bit.out == 1  # Still no change
+    
+    bit.clock = 0
+    assert bit.out == 1  # Still no change
+    
+    bit.load = 1
+    bit.in_ = 0
+    assert bit.out == 1  # Update, but not exposed yet
+    
+    bit.clock = 1
+    assert bit.out == 1  # No change
+    
+    bit.clock = 0
+    assert bit.out == 0  # Now you can see the new value
