9692f5184131c469711da3ef718f2158fd129335
Moss Prescott <moss@theprescotts.com>
2019-11-07 19:33:09 -0500

    Coarse and fine tests for DFF and Bit
    Based on whether or not they take note of the state of outputs in the _middle_ of a cycle (i.e. when clock is high)

--- test_03.py
+++ test_03.py
@@ -1,6 +1,30 @@
 from project_03 import *
 
-def test_dff():
+def test_dff_coarse():
+    """Test of DFF behavior when inputs are always stable across clock cycles.
+    """
+    
+    dff = run(DFF)
+    
+    assert dff.out == 0
+    
+    dff.in_ = 1
+    assert dff.out == 0
+
+    ticktock(dff)
+    assert dff.out == 1
+
+    dff.in_ = 0
+    assert dff.out == 1
+    
+    ticktock(dff)
+    assert dff.out == 0
+
+
+def test_dff_fine():
+    """Test of DFF behavior with varying signal timing.
+    """
+    
     dff = run(DFF)
     
     assert dff.out == 0
@@ -29,10 +53,10 @@ def test_dff():
     assert dff.out == 0
     
     dff.clock = 0
-    assert dff.out == 0
+    assert dff.out == 1  # New value appears on the falling edge, still
     
     dff.clock = 1
-    assert dff.out == 1  # New value appears on the falling edge, still
+    assert dff.out == 1
 
     dff.clock = 0
     assert dff.out == 1
@@ -43,7 +67,32 @@ def test_dff():
     # global), and providing tick() and tock() to cycle it.
     
 
-def test_bit():
+def test_bit_coarse():
+    bit = run(Bit)
+    
+    assert bit.out == 0
+    
+    bit.load = 1
+    bit.in_ = 1
+    assert bit.out == 0  # New value not visible on the output yet
+
+    ticktock(bit)
+    assert bit.out == 1  # Now you can see the new value
+
+    bit.load = 0
+    bit.in_ = 0
+    ticktock(bit)
+    assert bit.out == 1  # No change
+    
+    bit.load = 1
+    bit.in_ = 0
+    assert bit.out == 1  # Not updated yet
+    
+    ticktock(bit)
+    assert bit.out == 0  # Now you can see the new value
+
+
+def test_bit_fine():
     bit = run(Bit)
     
     assert bit.out == 0
@@ -77,3 +126,46 @@ def test_bit():
     
     bit.clock = 0
     assert bit.out == 0  # Now you can see the new value
+
+
+def test_register():
+    reg = run(Register)
+    
+    reg.in_ = 0
+    reg.load = 0
+    ticktock(reg)
+    assert reg.out == 0
+
+    reg.load = 1
+    ticktock(reg)
+    assert reg.out == 0
+
+    reg.in_ = -32123
+    reg.load = 0
+    ticktock(reg)
+    assert reg.out == 0
+    
+    reg.in_ = 11111
+    reg.load = 0
+    ticktock(reg)
+    assert reg.out == 0
+    
+    reg.in_ = -32123
+    reg.load = 1
+    ticktock(reg)
+    assert reg.out == -32123
+    
+
+def tick(inst):
+    inst._vector._propagate()  # HACK
+    inst.clock = 1
+    inst._vector._propagate()  # HACK
+
+def tock(inst):
+    inst._vector._propagate()  # HACK
+    inst.clock = 0
+    inst._vector._propagate()  # HACK
+
+def ticktock(inst):
+    tick(inst)
+    tock(inst)
