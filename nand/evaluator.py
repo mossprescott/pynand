348aa979104cc0cab1adcafba4b98991fc4c497e
Moss Prescott <moss@theprescotts.com>
2019-11-11 14:40:01 -0500

    Add flip_flops to NandVector

--- nand/evaluator.py
+++ nand/evaluator.py
@@ -4,30 +4,31 @@ class NandVector:
     """Tracks the true/false state of each of a set of bits, related by a sequence of Nand ops.
 
     Each value is represented as a single bit in a vector represented as an `int`.
-    
-    `inputs` maps the name/id of each input to a bit vector which is the location of the 
+
+    `inputs` maps the name/id of each input to a bit vector which is the location of the
     corresponding bit. `outputs` and `internal` have the same structure. Typically the three
-    sets of bits are disjoint, but that's not strictly required. 
-    
-    `ops` is a list of tuples (in_bits, out_bit). When any input changes, each op is applied in 
-    sequence (via `_nand_bits`), with just three bit-wise operations on the ints. In case there are 
-    any out-of-order or circular dependencies, the entire loop is repeated until no more changes are 
+    sets of bits are disjoint, but that's not strictly required.
+
+    `ops` is a list of tuples (in_bits, out_bit). When any input changes, each op is applied in
+    sequence (via `_nand_bits`), with just three bit-wise operations on the ints. In case there are
+    any out-of-order or circular dependencies, the entire loop is repeated until no more changes are
     seen (or, if no fixed-point is found, an exception is raised.)
     """
-    
-    def __init__(self, inputs, outputs, internal, ops):
+
+    def __init__(self, inputs, outputs, internal, ops, flip_flops):
         self.inputs = inputs
         self.outputs = outputs
         self.internal = internal
         self.ops = ops
-        
+        self.flip_flops = flip_flops
+
         self.traces = 0
         self.dirty = True
-        
+
     def set(self, key, value):
         """Set the value of an input bit identified by key (found in `inputs`).
         """
-        
+
         if value:
             self.traces |= self.inputs[key]
         else:
@@ -37,19 +38,19 @@ class NandVector:
     def get(self, key):
         """Get the value of an output bit identified by key (found in `outputs`).
         """
-        
+
         self._propagate()
-        
+
         return bool(self.traces & self.outputs[key])
-        
+
     def get_internal(self, key):
         """Get the value of an internal bit identified by key (found in `internal`).
         """
-        
+
         self._propagate()
-        
+
         return bool(self.traces & self.internal[key])
-        
+
     def _propagate(self):
         if not self.dirty: return
 
@@ -59,16 +60,27 @@ class NandVector:
             return ts
 
         self.traces = fixed_point(f, self.traces, limit=2)
-        
+
         self.dirty = False
+
+
+    # TODO: unify this with the state of the clock (not otherwise known here at present)
+    def _flop(self):
+        """Simulate advancing the clock, by copying the input of each flip-flop to its output.
+        """
         
+        self._propagate()
         
+        for in_bit, out_bit in self.flip_flops:
+            self.traces = copy_bit(in_bit, out_bit, self.traces)
+
+
 def nand_bits(in_bits, out_bits, traces):
-    """Apply NAND to the bits identified in `in_bits`, and store the result in the bit(s) identified 
+    """Apply NAND to the bits identified in `in_bits`, and store the result in the bit(s) identified
     in `out_bits`.
-    
-    If _all_ of the bits which are 1 in `in_bits` are also 1 in `traces`, then the result is `traces`, 
-    modified so that all the bits of `out_bits` are cleared (i.e. "not-and"). Otherwise, those same 
+
+    If _all_ of the bits which are 1 in `in_bits` are also 1 in `traces`, then the result is `traces`,
+    modified so that all the bits of `out_bits` are cleared (i.e. "not-and"). Otherwise, those same
     bits are set.
     """
     if (traces & in_bits) == in_bits:
@@ -76,6 +88,16 @@ def nand_bits(in_bits, out_bits, traces):
     else:
         return traces | out_bits
 
+
+def copy_bit(in_bit, out_bit, traces):
+    """Copy a single bit located at in_bit to out_bit. Actually, this is complementary to nand_bits (i.e. 
+    and_bits), but here it's used in a different way.
+    """
+    if traces & in_bit == in_bit:
+        return traces | out_bit
+    else:
+        return traces & ~out_bit
+    
 
 def fixed_point(f, x, limit=50):
     for i in range(limit):
@@ -88,5 +110,5 @@ def fixed_point(f, x, limit=50):
             x = tmp
     else:
         raise Exception(f"state did not settle after {limit} loops")
-    
+
 
--- nand/test_evaluator.py
+++ nand/test_evaluator.py
@@ -9,7 +9,8 @@ def test_xor():
           (0b000101, 0b001000),  # Nand(a, nand) -> na
           (0b000110, 0b010000),  # Nand(b, nand) -> nb
           (0b011000, 0b100000)   # Nand(na, na) -> out
-        ]
+        ],
+        []
     )
 
     assert xor.get('out') == False
@@ -26,3 +27,21 @@ def test_xor():
     xor.set('b', False)
     assert xor.get('out') == False
     assert xor.get_internal('na') and xor.get_internal('nb')
+
+
+def test_flop():
+    ff = NandVector(
+        {'in_': 0b01},
+        {'out': 0b10},
+        {},
+        [],
+        [(0b01, 0b10)])
+    
+    assert ff.get('out') == 0
+    
+    ff.set('in_', 1)
+    assert ff.get('out') == 0
+    
+    ff._flop()
+    assert ff.get('out') == 1
+    
