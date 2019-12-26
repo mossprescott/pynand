7f41cab28ac1b408eb73a4d6b6534f1fdfcdaebb
Moss Prescott <moss@theprescotts.com>
2019-12-25 20:13:14 -0500

    Implement synthesize (for already-flat ICs)

--- nand_new/integration.py
+++ nand_new/integration.py
@@ -1,36 +1,40 @@
+import collections
+# import itertools
+
+from nand.evaluator import NandVector
 from nand_new.component import Component
 
 class IC:
-    """An integrated circuit assembles one or more components by recording how their 
+    """An integrated circuit assembles one or more components by recording how their
     inputs and outputs are connected.
-    
-    An IC also acts as a component when it is assembled with other components into a 
+
+    An IC also acts as a component when it is assembled with other components into a
     larger chip.
     """
-    
+
     def __init__(self, inputs, outputs):
         self.inputs = inputs
         self.outputs = outputs
         self.root = Root(self)
         self.components = {}
         self.wires = {}
-        
+
     def wire(self, from_output, to_input):
-        """Connect a single trace from an output of one component to an input of another. 
-        
+        """Connect a single trace from an output of one component to an input of another.
+
         Note that the IC's inputs act as outputs to feed the inputs of components, and vice versa.
         Yes, that does seem confusing.
-        
-        Each input can be connected to exactly one output; an output may feed any number of 
+
+        Each input can be connected to exactly one output; an output may feed any number of
         inputs, or none at all. A new wiring overwrites any previous wiring to the same input.
-        
+
         Both components become part of this circuit.
-        
+
         The connection is checked on both ends to ensure that it specifies a valid name and bit.
-        Note: if a WiringError is thrown, both components are nevertheless tracked as being part 
+        Note: if a WiringError is thrown, both components are nevertheless tracked as being part
         of the IC. TODO: maybe clean that up after the fact?
         """
-        
+
         self._add_component(from_output.comp)
         self._add_component(to_input.comp)
 
@@ -42,30 +46,94 @@ class IC:
             raise WiringError(f"Component {self._comp_label(to_input.comp)} has no input '{to_input.name}'")
         elif to_input.bit < 0 or to_input.bit >= to_input.comp.inputs()[to_input.name]:
             raise WiringError(f"Tried to connect bit {to_input.bit} of {to_input.comp.inputs()[to_input.name]}-bit input {self._comp_label(to_input.comp)}.{to_input.name}")
-            
+
         self.wires[to_input] = from_output
-        
+
     def _add_component(self, comp):
         if comp != self.root and comp not in self.components:
             self.components[comp] = len(self.components)
-        
+
     def _comp_label(self, comp):
         if comp == self.root:
             return "Root"
         else:
             return f"{comp.__class__.__name__}_{self.components[comp]}"
-        
+
     def flatten(self):
         """Construct a new IC which has the same structure as this one, but no nested ICs.
         That is, the wiring of all child ICs has been "inlined" into a single flat assembly.
         """
-        raise NotImplementedError
+        # TODO
+        return self
+
+    def synthesize(self):
+        """Compile the chip down to traces and ops for evaluation.
+
+        Returns a NandVector.
+        """
+
+        flat = self.flatten()
+
+        # check for missing wires?
+        # check for unused components?
+
+        # Assign a bit for each output connection:
+        next_bit = 0
+        all_bits = {}
+        for conn in set(self.wires.values()): # TODO: sort by the order the components were added?
+            all_bits[conn] = next_bit
+            next_bit += 1
+        print(f"all_bits: {all_bits}")
+
+        # Construct map of IC inputs, directly from all_bits:
+        inputs = {
+            (name, bit): 1 << all_bits[Connection(self.root, name, bit)]
+            for name, bits in self.inputs.items()
+            for bit in range(bits)  # TODO: None if single bit?
+        }
+        print(f"inputs: {inputs}")
+
+        # Construct map of IC ouputs, mapped to all_bits via wires:
+        outputs = {
+            (name, bit): 1 << all_bits[self.wires[Connection(self.root, name, bit)]]
+            for name, bits in self.outputs.items()
+            for bit in range(bits)  # TODO: None if single bit?
+        }
+        print(f"outputs: {outputs}")
+
+        internal = {}  # TODO
+
+        # Construct a map of the traces for each single component, and ask it for its ops:
+        # TODO: sort components topologically based on wires, starting with Root?
+        ops = []
+        for comp in self.components:
+            traces = {}
+            for name, bits in comp.inputs().items():
+                traces[name] = [1 << all_bits[self.wires[Connection(comp, name, bit)]] for bit in range(bits)]
+            for name, bits in comp.outputs().items():
+                traces[name] = [1 << all_bits[Connection(comp, name, bit)] for bit in range(bits)]
+            cops = comp.combine(**traces)
+            sops = comp.sequence(**traces)
+            # TEMP: need to refactor NandVector a little
+            class Op:
+                def propagate(self, traces):
+                    for f in cops:
+                        traces = f(traces)
+                    return traces
+                def flop(self, traces):
+                    for f in sops:
+                        traces = f(traces)
+                    return traces
+            ops.append(Op())
+
+
+        return NandVector(inputs, outputs, internal, ops)
 
 
 class Root:
     """Pseudo-component providing access to an ICs inputs and outputs (under the opposite names).
     """
-    
+
     def __init__(self, ic):
         self.ic = ic
 
@@ -74,17 +142,11 @@ class Root:
 
     def outputs(self):
         return self.ic.inputs
-        
 
-class Connection:
-    def __init__(self, comp, name, bit):
-        # assert 0 <= bit < 16  # wait and signal a WiringError
-        self.comp = comp
-        self.name = name
-        self.bit = bit
-    
-    
+
+Connection = collections.namedtuple('Connection', ('comp', 'name', 'bit'))
+
+
 class WiringError(Exception):
     def __init__(self, msg):
         Exception(msg)
-    
--- nand_new/test_integration.py
+++ nand_new/test_integration.py
@@ -4,39 +4,59 @@ from nand_new.component import Nand
 from nand_new.integration import IC, Connection, WiringError
 
 def test_simple_wiring():
-    nott = IC({"in_": 1}, {"out": 1})
-    
+    ic = IC({"in_": 1}, {"out": 1})
+
     nand = Nand()
-    
-    nott.wire(Connection(nott.root, "in_", 0), Connection(nand, "a", 0))
-    
-    # print(f"{nott.components}")
+
+    ic.wire(Connection(ic.root, "in_", 0), Connection(nand, "a", 0))
+
+    # print(f"{ic.components}")
     # assert 0
 
     # TODO: assert something
-        
-    
+
+
 def test_wiring_errors():
-    nott = IC({"in_": 1}, {"out": 1})
-    
+    ic = IC({"in_": 1}, {"out": 1})
+
     nand = Nand()
-    
+
     with pytest.raises(WiringError) as exc_info:
-        nott.wire(Connection(nott.root, "in_", 0), Connection(nand, "foo", 0))
+        ic.wire(Connection(ic.root, "in_", 0), Connection(nand, "foo", 0))
     assert str(exc_info.value) == "Component Nand_0 has no input 'foo'"
-    
+
     with pytest.raises(WiringError) as exc_info:
-        nott.wire(Connection(nott.root, "in_", 0), Connection(nand, "a", 1))
+        ic.wire(Connection(ic.root, "in_", 0), Connection(nand, "a", 1))
     assert str(exc_info.value) == "Tried to connect bit 1 of 1-bit input Nand_0.a"
-    
+
     with pytest.raises(WiringError) as exc_info:
-        nott.wire(Connection(nott.root, "in_", 0), Connection(nand, "a", -1))
+        ic.wire(Connection(ic.root, "in_", 0), Connection(nand, "a", -1))
     assert str(exc_info.value) == "Tried to connect bit -1 of 1-bit input Nand_0.a"
-    
+
     with pytest.raises(WiringError) as exc_info:
-        nott.wire(Connection(nott.root, "bar", 0), Connection(nand, "a", 0))
+        ic.wire(Connection(ic.root, "bar", 0), Connection(nand, "a", 0))
     assert str(exc_info.value) == "Component Root has no output 'bar'"
-    
+
     with pytest.raises(WiringError) as exc_info:
-        nott.wire(Connection(nott.root, "in_", 17), Connection(nand, "a", 0))
+        ic.wire(Connection(ic.root, "in_", 17), Connection(nand, "a", 0))
     assert str(exc_info.value) == "Tried to connect bit 17 of 1-bit output Root.in_"
+
+
+def test_simple_synthesis():
+    ic = IC({"a": 1, "b": 1}, {"out": 1})
+    nand = Nand()
+    ic.wire(Connection(ic.root, "a", 0), Connection(nand, "a", 0))
+    ic.wire(Connection(ic.root, "b", 0), Connection(nand, "b", 0))
+    ic.wire(Connection(nand, "out", 0), Connection(ic.root, "out", 0))
+    nv = ic.synthesize()
+
+    assert nv.get(("out", 0)) == True
+
+    nv.set(("a", 0), True)
+    assert nv.get(("out", 0)) == True
+
+    nv.set(("b", 0), True)
+    assert nv.get(("out", 0)) == False
+
+    nv.set(("a", 0), False)
+    assert nv.get(("out", 0)) == True
