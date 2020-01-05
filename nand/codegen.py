d3e3fd13e72db2e7602209ffafb9a1df503f952d
Moss Prescott <moss@theprescotts.com>
2020-01-05 17:01:25 -0500

    Actually generate code, for Nands only

--- nand/codegen.py
+++ nand/codegen.py
@@ -1,29 +1,58 @@
+from nand.component import Nand
+from nand.integration import Connection, root
 from nand.optimize import simplify
+
+PRIMITIVES = set([
+    "Nand"
+])
 
 def translate(ic):
     """Given an IC, generate the Python source of a class which implements the chip, as a sequence of lines."""
 
-    flat = simplify(ic.flatten())
+    flat = simplify(ic.flatten(primitives=PRIMITIVES))
+    all_comps = flat.sorted_components()
+
+    lines = []
+    def l(indent, str):
+        lines.append("    "*indent + str)
+
+    def src(conn):
+        # TODO: deal with lots of cases
+        if conn.comp == root:
+            return f"self._{conn.name}"
+        else:
+            return f"_{all_comps.index(conn.comp)}_{conn.name}"
 
     class_name = f"{ic.label}_gen"
-    lines = [
-        f"class {class_name}(Chip):",
-        f"    def __init__(self):",
-        f"        Chip.__init__(self, {ic.inputs()!r}, {ic.outputs()!r})",
+    l(0, f"class {class_name}(Chip):")
+    l(1,   f"def __init__(self):")
+    l(2,     f"Chip.__init__(self, {ic.inputs()!r}, {ic.outputs()!r})")
+    for name in flat.inputs():
+        l(2, f"self._{name} = 0")
+    for name in flat.outputs():
+        l(2, f"self._{name} = 0")
+    l(0, "")
+    
+    l(1, f"def _combine(self):"),
+    for comp in all_comps:
+        if isinstance(comp, Nand):
+            a_conn = ic.wires[Connection(comp, "a", 0)]
+            b_conn = ic.wires[Connection(comp, "b", 0)]
+            l(2, f"_{all_comps.index(comp)}_out = not ({src(a_conn)} and {src(b_conn)})")
+        else:
+            raise Exception(f"Unrecognized primitive: {comp}")
+    for name, bits in flat.outputs().items():
+        # TODO: handle bits > 1
+        conn = flat.wires[Connection(root, name, 0)]
+        l(2, f"self._{name} = {src(conn)}")
+    l(0, "")
+    
+    l(1, f"def _sequence(self):")
         # TODO
-        f"        self._a = 0",
-        f"        self._b = 0",
-        f"        self._out = 0",
-        f"",
-        f"    def _combine(self):",
-        # TODO
-        f"        _0 = not (self._a and self._b)",
-        f"        self._out = _0",
-        f"",
-        f"    def _sequence(self):",
-        # TODO
-        f"        pass",
-    ]
+    l(2, f"pass")
+    
+    print(flat)
+    print('\n'.join(lines))
 
     eval(compile('\n'.join(lines),
             filename="<generated>",
--- nand/test_codegen.py
+++ nand/test_codegen.py
@@ -1,22 +1,51 @@
 from nand.codegen import translate
+from nand.component import Nand
 from nand.integration import IC, Connection, root
-from nand.syntax import Nand, _constr
-# from project_01 import Not
+import nand.syntax
 
-    
-def test_Nand():
-    ic = _constr(Nand)
 
-    klass = translate(ic)
-    nand = klass()
+def test_nand():
+    ic = nand.syntax._constr(nand.syntax.Nand)
 
-    assert nand.out == True
+    nnd = translate(ic)()
 
-    nand.a = True
-    assert nand.out == True
+    assert nnd.out == True
 
-    nand.b = True
-    assert nand.out == False
+    nnd.a = True
+    assert nnd.out == True
 
-    nand.a = False
-    assert nand.out == True
+    nnd.b = True
+    assert nnd.out == False
+
+    nnd.a = False
+    assert nnd.out == True
+
+
+def test_and3():
+    """A simple component that's definitely not handled as a primitive."""
+
+    ic = IC("And3", {"a": 1, "b": 1, "c": 1}, {"out": 1})
+    nand1 = Nand()
+    ic.wire(Connection(root, "a", 0), Connection(nand1, "a", 0))
+    ic.wire(Connection(root, "b", 0), Connection(nand1, "b", 0))
+    nand2 = Nand()
+    ic.wire(Connection(nand1, "out", 0), Connection(nand2, "a", 0))
+    ic.wire(Connection(nand1, "out", 0), Connection(nand2, "b", 0))
+    nand3 = Nand()
+    ic.wire(Connection(nand2, "out", 0), Connection(nand3, "a", 0))
+    ic.wire(Connection(root, "c", 0), Connection(nand3, "b", 0))
+    nand4 = Nand()
+    ic.wire(Connection(nand3, "out", 0), Connection(nand4, "a", 0))
+    ic.wire(Connection(nand3, "out", 0), Connection(nand4, "b", 0))
+    ic.wire(Connection(nand4, "out", 0), Connection(root, "out", 0))
+
+    and3 = translate(ic)()
+
+    assert and3.out == False
+
+    for i in range(8):
+        a, b, c = [bool(i & (1 << j)) for j in range(3)]
+        and3.a = a
+        and3.b = b
+        and3.c = c
+        assert and3.out == (a and b and c)
