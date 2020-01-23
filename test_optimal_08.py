bfdf4af0aeff6c175809e2b5e3bec1c4a3fefc57
Moss Prescott <moss@theprescotts.com>
2020-01-22 20:20:00 -0500

    Tests for code size and cycle count

---
+++ test_optimal_08.py
@@ -0,0 +1,53 @@
+import os
+
+import nand.syntax
+from nand.translate import translate_dir
+from project_05 import Computer
+from project_06 import assemble
+from project_07 import translate_line
+from project_08 import Translator
+
+
+def test_code_size_pong():
+    """A simple translator will work for small programs, but won't be able to fit a real program in the ROM."""
+
+    translate = Translator()
+    
+    translate.preamble()
+
+    translate_dir(translate, translate_line, "examples/Pong")
+    translate_dir(translate, translate_line, "nand2tetris/tools/OS")  # HACK not committed
+
+    # for instr in translate.asm:
+    #     print(instr)
+    print(f"instructions: {translate.asm.instruction_count:0,d}")
+
+    # Note: the nand2tetris translator does it in about 27,500
+    assert translate.asm.instruction_count <= 2**15
+    assert False
+
+
+def test_cycles_to_init():
+    """Count the number of cycles executed before Main.main is called."""
+
+    translate = Translator()
+
+    translate.preamble()
+
+    translate_dir(translate, translate_line, "examples/Draw")
+    translate_dir(translate, translate_line, "nand2tetris/tools/OS")  # HACK not committed
+
+    computer = nand.syntax.run(Computer, simulator='codegen')
+    asm = assemble(translate.asm)
+    computer.init_rom(asm)
+
+    cycles = 0
+    while True:
+        computer.ticktock()
+        if translate.asm.src_map.get(computer.pc) == 'function Main.main 1':
+            break
+        elif cycles > 10_000_000:
+            assert False, "Ran for 10 million cycles without reaching Main.main"
+        cycles += 1
+
+    assert cycles < 5_000_000
