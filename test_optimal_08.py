a95e7efaf90722f95802690c8266d172acba0866
Moss Prescott <moss@theprescotts.com>
2020-01-31 12:06:21 -0500

    Remove some hacks

--- test_optimal_08.py
+++ test_optimal_08.py
@@ -4,9 +4,11 @@ import nand.syntax
 from nand.translate import translate_dir
 from project_05 import Computer
 from project_06 import assemble
-from project_07 import translate_line
+from project_07 import parse_line
 from project_08 import Translator
 
+# from nand.translate import AssemblySource
+# from nand.solutions import solved_07
 
 def test_code_size_pong():
     """A simple translator will work for small programs, but won't be able to fit a real program in the ROM."""
@@ -15,8 +17,8 @@ def test_code_size_pong():
     
     translate.preamble()
 
-    translate_dir(translate, translate_line, "examples/Pong")
-    translate_dir(translate, translate_line, "nand2tetris/tools/OS")  # HACK not committed
+    translate_dir(translate, parse_line, "examples/Pong")
+    translate_dir(translate, parse_line, "nand2tetris/tools/OS")  # HACK not committed
 
     # for instr in translate.asm:
     #     print(instr)
@@ -24,18 +26,18 @@ def test_code_size_pong():
 
     # Note: the nand2tetris translator does it in about 27,500
     assert translate.asm.instruction_count <= 2**15
-    assert False
 
 
 def test_cycles_to_init():
     """Count the number of cycles executed before Main.main is called."""
 
     translate = Translator()
-
+    # translate = solved_07.Translator(AssemblySource())
+    
     translate.preamble()
 
-    translate_dir(translate, translate_line, "examples/Draw")
-    translate_dir(translate, translate_line, "nand2tetris/tools/OS")  # HACK not committed
+    translate_dir(translate, parse_line, "examples/Draw")
+    translate_dir(translate, parse_line, "nand2tetris/tools/OS")  # HACK not committed
 
     computer = nand.syntax.run(Computer, simulator='codegen')
     asm = assemble(translate.asm)
@@ -50,4 +52,6 @@ def test_cycles_to_init():
             assert False, "Ran for 10 million cycles without reaching Main.main"
         cycles += 1
 
+    print(f"cycles to init: {cycles:0,d}")
+    
     assert cycles < 5_000_000
