7d33906217e5df57795a4a3b34563eb968838303
Moss Prescott <moss@theprescotts.com>
2020-02-10 13:55:11 -0500

    Another alternative CPU: “threaded”

---
+++ alt/test_threaded.py
@@ -0,0 +1,180 @@
+import pytest
+
+from nand import run, unsigned
+from nand.translate import translate_dir
+import test_05
+import test_06
+import test_07
+import test_08
+import test_optimal_08
+
+from alt.threaded import *
+
+#
+# First test that the new CPU executes all Hack instructions as expected:
+#
+
+def test_backward_compatible_cpu():
+    test_05.test_cpu(ThreadedCPU)
+
+def test_backward_compatible_computer_add():
+    test_05.test_computer_add(ThreadedComputer)
+    
+def test_backward_compatible_computer_max():
+    test_05.test_computer_max(ThreadedComputer)
+    
+def test_backward_compatible_speed():
+    test_05.test_speed(ThreadedComputer)
+
+
+#
+# Components:
+#
+def test_eq16():
+    assert run(Eq16, a=1, b=0).out == False
+    assert run(Eq16, a=0, b=0).out == True
+    assert run(Eq16, a=12345, b=12345).out == True
+    assert run(Eq16, a=-23456, b=-23456).out == True
+    assert run(Eq16, a=-32768, b=-32768).out == True
+    for i in range(16):
+        assert run(Eq16, a=(1 << i), b=(1 << i)).out == True
+        assert run(Eq16, a=(1 << i), b=       0).out == False
+        assert run(Eq16, a=       0, b=(1 << i)).out == False
+
+    # Ouch. Probably could do it with less if was smarter.
+    assert gate_count(Eq16) == {'nands': 110}
+
+
+#
+# Now some tests of added stuff:
+#
+
+@pytest.mark.parametrize("simulator", ["vector", "codegen"])
+def test_call_and_return(simulator):
+    """CPU jumps and returns using two new instructions and a new register."""
+
+    symbols = {"target": 12345}
+
+    cpu = run(ThreadedCPU, simulator=simulator)
+
+    # init PC to 1000
+    cpu.instruction = parse_op("@1000")
+    cpu.ticktock()
+    cpu.instruction = parse_op("0;JMP")
+    cpu.ticktock()
+
+    cpu.instruction = parse_op("CALL target", symbols)
+    cpu.ticktock()
+    
+    assert cpu.pc == 12345
+
+    cpu.instruction = parse_op("RTN")
+    cpu.ticktock()
+    assert cpu.pc == 1001    
+
+
+def test_computer_gates():
+   assert gate_count(ThreadedComputer) == {
+       'nands': 1544,  # ??? compare to 1262
+       'dffs': 64,  # 4 registers
+       'roms': 1,
+       'rams': 2,
+       'inputs': 1,
+   }
+
+
+#
+# Test that all Hack instructions are assembled the same way:
+#
+
+def test_backward_compatible_asm_ops_add():
+    test_06.test_asm_ops_add(parse_op)
+
+def test_backward_compatible_asm_ops_max():
+    test_06.test_asm_ops_max(parse_op)
+
+def test_backward_compatible_ops():
+    test_06.test_ops(parse_op)
+
+
+#
+# Test new instructions:
+#
+
+def test_assemble_call():
+    symbols = {"target": 12345}
+    assert unsigned(parse_op("CALL target", symbols))  == 0b1000_0000_0000_0000 | 12345
+    
+    # with pytest.raises(SyntaxError) as exc_info:
+    #     parse_op("CALL ")
+    # assert str(exc_info.value).startswith("M not allowed as a destination for pop")
+
+def test_assemble_return():
+    assert unsigned(parse_op("RTN"))  == 0b1000_0000_0000_0000
+
+
+#
+# VM translator:
+#
+
+def test_vm_simple_add():
+    test_07.test_simple_add(chip=ThreadedComputer, assemble=assemble, translator=Translator)
+    assert False
+
+def test_vm_stack_ops():
+    test_07.test_stack_ops(chip=ThreadedComputer, assemble=assemble, translator=Translator)
+
+def test_vm_memory_access_basic():
+    test_07.test_memory_access_basic(chip=ThreadedComputer, assemble=assemble, translator=Translator)
+
+def test_vm_memory_access_pointer():
+    test_07.test_memory_access_pointer(chip=ThreadedComputer, assemble=assemble, translator=Translator)
+
+def test_vm_memory_access_static():
+    test_07.test_memory_access_static(chip=ThreadedComputer, assemble=assemble, translator=Translator)
+
+
+def test_vm_basic_loop():
+    test_08.test_basic_loop(chip=ThreadedComputer, assemble=assemble, translator=Translator)
+
+def test_vm_fibonacci_series():
+    test_08.test_fibonacci_series(chip=ThreadedComputer, assemble=assemble, translator=Translator)
+    
+def test_vm_simple_function():
+    test_08.test_simple_function(chip=ThreadedComputer, assemble=assemble, translator=Translator)
+    
+def test_vm_nested_call():
+    test_08.test_nested_call(chip=ThreadedComputer, assemble=assemble, translator=Translator)
+
+def test_vm_fibonacci_element():
+    test_08.test_fibonacci_element(chip=ThreadedComputer, assemble=assemble, translator=Translator)
+    assert False
+
+def test_vm_statics_multiple_files():
+    test_08.test_statics_multiple_files(chip=ThreadedComputer, assemble=assemble, translator=Translator)
+
+
+def test_vm_pong_instructions():
+    instruction_count = test_optimal_08.count_pong_instructions(Translator)
+    
+    # compare to the project_08 solution (about 28k)
+    assert instruction_count < -1  # 15_749
+
+
+def test_vm_cycles_to_init():
+    cycles = test_optimal_08.count_cycles_to_init(ThreadedComputer, assemble, Translator)
+
+    # compare to the project_08 solution (about 4m)
+    assert cycles < -1  # 2_612_707
+    
+
+# def init_sp(cpu):
+#     ASM = [
+#         "@256",
+#         "D=A",
+#         "@0",
+#         "M=D",
+#     ]
+#     for instr in ASM:
+#         cpu.instruction = parse_op(instr)
+#         cpu.ticktock()
---
+++ alt/threaded.py
@@ -0,0 +1,288 @@
+"""An alternative CPU which enables _much_ smaller binaries, and therefore much larger programs, by 
+making a simple non-recursive call/return cost only one instruction in ROM per occurrence. With that,
+a "threaded" interpreter is much more compact (and readable).
+
+A register is added to hold the return address, leaving D and A available for arguments.
+
+CALL [symbol]
+- bit pattern: 10xx_xxxx_xxxx_xxxx
+- symbol is resolved to a location in ROM, which must be non-zero and fit in 14 bits (the first half of ROM).
+- RA <- the address of the next instruction (i.e. PC+1)
+- PC <- the resolved address for symbol from instr[0..14]
+
+RTN
+- bit pattern: 1000_0000_0000_0000
+- PC <- RA
+
+Typical use:
+  @10
+  CALL VM.push_constant
+  @3
+  CALL VM.push_local
+  CALL VM.add
+  CALL VM.pop_local_0  # Probably special-case some common arguments
+
+Interpreter example:
+(VM.push_constant)
+  // move arg to D
+  D=A
+  // familiar push_d sequence:
+  @SP
+  M=M+1
+  A=M-1
+  M=D
+  // return
+  RTN
+
+That's 2 instructions in ROM and 8 at runtime for pushing a large constant. Compare to 6 each in the standard VM.
+For small values, down to 1 (6 at runtime) from 4.
+
+Other opcodes:
+- gotos don't return, so only one instruction in ROM and one added at runtime.
+- call needs an address and arg count, so two or three more instructions to setup, total of 4 or 5 (compare to 13.)
+"""
+
+import re
+
+from nand import *
+from nand.translate import AssemblySource, translate_dir
+
+from nand.solutions.solved_01 import And, Or, Not, Xor, Not16, Mux16
+from nand.solutions.solved_02 import Inc16, ALU
+from nand.solutions.solved_02 import Zero16  # HACK: should be defined in project_02?
+from nand.solutions.solved_03 import Register
+from nand.solutions.solved_05 import MemorySystem, PC
+from nand.solutions import solved_06
+from nand.solutions import solved_07
+
+
+# Compare two 16-bit values. Another thing that's easy to simulate in codegen, and seems like it 
+# _should_ have a reasonably efficient representation in Nands, even if this isn't it.
+# Note: this simplifies to Zero16, if one of the inputs is 0, so maybe should just implement this 
+# in project_02 instead.
+def mkEq16(inputs, outputs):
+    a = inputs.a
+    b = inputs.b
+
+    outputs.out = And(
+        a=And(a=And(a=And(a=Not(in_=Xor(a=a[15], b=b[15]).out).out,
+                          b=Not(in_=Xor(a=a[14], b=b[14]).out).out).out,
+                    b=And(a=Not(in_=Xor(a=a[13], b=b[13]).out).out,
+                          b=Not(in_=Xor(a=a[12], b=b[12]).out).out).out).out,
+              b=And(a=And(a=Not(in_=Xor(a=a[11], b=b[11]).out).out,
+                          b=Not(in_=Xor(a=a[10], b=b[10]).out).out).out,
+                    b=And(a=Not(in_=Xor(a=a[ 9], b=b[ 9]).out).out,
+                          b=Not(in_=Xor(a=a[ 8], b=b[ 8]).out).out).out).out).out,
+        b=And(a=And(a=And(a=Not(in_=Xor(a=a[ 7], b=b[ 7]).out).out,
+                          b=Not(in_=Xor(a=a[ 6], b=b[ 6]).out).out).out,
+                    b=And(a=Not(in_=Xor(a=a[ 5], b=b[ 5]).out).out,
+                          b=Not(in_=Xor(a=a[ 4], b=b[ 4]).out).out).out).out,
+              b=And(a=And(a=Not(in_=Xor(a=a[ 3], b=b[ 3]).out).out,
+                          b=Not(in_=Xor(a=a[ 2], b=b[ 2]).out).out).out,
+                    b=And(a=Not(in_=Xor(a=a[ 1], b=b[ 1]).out).out,
+                          b=Not(in_=Xor(a=a[ 0], b=b[ 0]).out).out).out).out).out).out
+Eq16 = build(mkEq16)
+
+
+def mkMask15(inputs, outputs):
+    for i in range(15):
+        outputs.out[i] = inputs.in_[i]
+    outputs.out[15] = Not(in_=1).out  # HACK: syntax not working for output bit, apparently
+
+Mask15 = build(mkMask15)
+
+
+def mkThreadedCPU(inputs, outputs):
+    inM = inputs.inM                 # M value input (M = contents of RAM[A])
+    instruction = inputs.instruction # Instruction for execution
+    reset = inputs.reset             # Signals whether to re-start the current
+                                     # program (reset==1) or continue executing
+                                     # the current program (reset==0).
+
+    i, cr, _, a, c5, c4, c3, c2, c1, c0, da, dd, dm, jlt, jeq, jgt = [instruction[j] for j in reversed(range(16))]
+
+    not_i = Not(in_=i).out
+
+    call_rtn = And(a=i, b=Not(in_=cr).out).out
+    rtn = Eq16(a=instruction, b=0x8000).out
+    call = And(a=call_rtn, b=Not(in_=rtn).out).out
+    call_target = Mask15(in_=instruction).out
+    
+    alu = lazy()
+    pc = lazy()
+    a_reg = Register(in_=Mux16(a=instruction, b=alu.out, sel=i).out, load=Or(a=not_i, b=da).out)
+    d_reg = Register(in_=alu.out, load=And(a=i, b=dd).out)
+    ra_reg = Register(in_=pc.nxt, load=call)
+
+    jump_lt = And(a=alu.ng, b=jlt).out
+    jump_eq = And(a=alu.zr, b=jeq).out
+    jump_gt = And(a=And(a=Not(in_=alu.ng).out, b=Not(in_=alu.zr).out).out, b=jgt).out
+    jump = And(a=i,
+               b=Or(a=jump_lt, b=Or(a=jump_eq, b=jump_gt).out).out
+              ).out
+              
+    next_pc = Mux16(
+                a=Mux16(
+                    a=a_reg.out, 
+                    b=ra_reg.out,
+                    sel=rtn).out,
+                b=call_target, 
+                sel=call).out
+    
+    pc.set(PC(in_=next_pc, load=Or(a=jump, b=call_rtn).out, inc=1, reset=reset))
+    alu.set(ALU(x=d_reg.out, y=Mux16(a=a_reg.out, b=inM, sel=a).out,
+                zx=c5, nx=c4, zy=c3, ny=c2, f=c1, no=c0))
+
+
+    outputs.outM = alu.out                   # M value output
+    outputs.writeM = And(a=dm, b=i).out      # Write to M?
+    outputs.addressM = a_reg.out             # Address in data memory (of M) (latched)
+    outputs.pc = pc.out                      # address of next instruction (latched)
+
+    # HACK
+    outputs.ra = ra_reg.out
+    outputs.rtn = rtn
+    outputs.call = call
+
+ThreadedCPU = build(mkThreadedCPU)
+
+
+def mkThreadedComputer(inputs, outputs):
+    reset = inputs.reset
+    
+    cpu = lazy()
+
+    rom = ROM(15)(address=cpu.pc)
+
+    mem = MemorySystem(in_=cpu.outM, load=cpu.writeM, address=cpu.addressM)
+
+    cpu.set(ThreadedCPU(inM=mem.out, instruction=rom.out, reset=reset))
+
+    # HACK: need some dependency to force the whole thing to be synthesized.
+    # Exposing the PC also makes it easy to observe what's happening in a dumb way.
+    outputs.pc = cpu.pc
+
+ThreadedComputer = build(mkThreadedComputer)
+
+
+def parse_op(string, symbols={}):
+    m = re.match(r"CALL ([^ ]+)", string)
+    if m:
+        return 0b1000_0000_0000_0000 | symbols[m.group(1)]
+
+    if string == "RTN":
+        return 0b1000_0000_0000_0000
+
+    return solved_06.parse_op(string, symbols)
+
+
+def assemble(lines):
+    return solved_06.assemble(lines, parse_op)
+
+
+
+
+class Translator:
+    """Can't really re-use anything from the standard translator. 
+    """
+    
+    def __init__(self):
+        self.asm = AssemblySource()
+
+        # Parameters controlling how many specialized opcode variants are emitted.
+        # May be manually tweaked. A smart translator would inspect the source and choose them 
+        # to optimize for size/speed.
+        self.SPECIALIZED_MAX_CALL_NUM_ARGS = 2
+
+        start = self.asm.next_label("start")
+        self.asm.instr(f"@{start}")
+        self.asm.instr("0;JMP")
+        
+        # "Microcoded" instructions, which for this translator basically includes _all_ opcodes,
+        # plus many special-cases:
+        # If there's a single argument, it's passed in A. If more than one, additional args are 
+        # passed in R13-R15. See each implementation for specifics.
+        self._library()
+        
+        # Early check that the library of opcodes fits in the first half of the ROM, as required.
+        # Practically speaking, probably want it to be _much_ smaller than that.
+        assert self.asm.instruction_count <= 2**14
+        
+        self.asm.label(start)
+
+    def preamble(self):
+        self.asm.start("VM initialization")
+        self.asm.instr("@256")
+        self.asm.instr("D=A")
+        self.asm.instr("@SP")
+        self.asm.instr("M=D")
+
+        self.call("Sys", "init", 0)
+    
+    def call(self, class_name, function_name, num_args):
+        """Callee address in A. num_args in R13 if not specialized.
+        """
+
+        self.asm.start(f"call {class_name}.{function_name} {num_args}")
+
+        if num_args <= self.SPECIALIZED_MAX_CALL_NUM_ARGS:
+            self.asm.instr(f"@{class_name}.{function_name}")
+            self.asm.instr(f"CALL VM.call_{num_args}")
+        else:
+            self.asm.instr(f"@{num_args}")
+            self.asm.instr(f"D=A")
+            self.asm.instr(f"@R13")
+            self.asm.instr(f"M=D")
+        
+            self.asm.instr(f"@{class_name}.{function_name}")
+            self.asm.instr(f"CALL VM.call")
+
+    def _library(self):
+        
+        # call
+        
+        for num_args in range(self.SPECIALIZED_MAX_CALL_NUM_ARGS+1):
+            self.asm.label(f"VM.call_{num_args}")
+            self.asm.instr(f"D=A")
+            self.asm.instr(f"@R14")
+            self.asm.instr(f"M=D")
+            self.asm.instr(f"@{num_args}")
+            self.asm.instr(f"D=A")
+            self.asm.instr(f"@R13")
+            self.asm.instr(f"M=D")
+            self.asm.instr(f"@VM.call_common")
+            self.asm.instr(f"0;JMP")
+
+        self.asm.label(f"VM.call")
+        # R14 = callee address
+        self.asm.instr(f"D=A")
+        self.asm.instr(f"@R14")
+        self.asm.instr(f"M=D")
+        # fall through to the common impl:
+
+        self.asm.label(f"VM.call_common")
+        # TODO...
+        self.asm.instr(f"@R14")
+        self.asm.instr(f"A=M")
+        self.asm.instr(f"0;JMP")
+
+
+if __name__ == "__main__":
+    TRACE = False
+
+    import sys
+    import computer
+
+    translate = Translator()
+    
+    translate.preamble()
+    
+    translate_dir(translate, solved_07.parse_line, sys.argv[1])
+    translate_dir(translate, solved_07.parse_line, "nand2tetris/tools/OS")  # HACK not committed
+    
+    # if TRACE:
+    if True:
+        for instr in translate.asm:
+            print(instr)
+
+    computer.run(assemble(translate.asm), chip=ThreadedComputer, src_map=translate.asm.src_map if TRACE else None)
