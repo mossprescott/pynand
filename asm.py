83e2815b56971038d86f25523ec6c0b0392dc06a
Moss Prescott <moss@theprescotts.com>
2020-02-18 08:44:23 -0500

    Remove not-very-useful command-line assembler

--- asm.py
+++
@@ -1,9 +0,0 @@
-"""Read assembly from stdin, write instructions to stdout, 16-binary digits per line."""
-
-import sys
-from project_06 import assemble
-
-for instr in assemble(sys.stdin):
-    for i in range(15, -1, -1):
-        print(int(instr & (1 << i) != 0), end='')
-    print()
