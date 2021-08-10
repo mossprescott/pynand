719f2e4d54e19e7977917160dc18b940606ae696
Moss Prescott <moss@theprescotts.com>
2021-08-10 15:03:18 -0400

    Implement Output; lots of related test wrangling

--- project_12.py
+++ project_12.py
@@ -2,11 +2,12 @@
 #
 # See https://www.nand2tetris.org/project12
 
+from re import sub
 from nand import parsing, jack_ast
 
 import project_10
 
-# SOLVERS: remove this import to get started
+# SOLVERS: remove this import when you've implemented all the tagged fragments
 from nand.solutions import solved_12
 
 
@@ -15,6 +16,21 @@ def parse_classVarDecs(src):
 
 def parse_subroutineDec(src):
     return project_10.SubroutineDecP.parse(project_10.lex(src))
+
+
+#
+# Array Library
+#
+# This is the simplest of the classes that make up the "OS" library.
+# Your implementation should require no state and just a line or two
+# per function.
+#
+
+# SOLVERS: You can define static and field variables here if you need them.
+ARRAY_CLASS_VARS = parse_classVarDecs("""
+    // static int ...;
+    // field int ...;
+""")
 
 
 # SOLVERS: un-comment and fill in the implementation
@@ -26,22 +42,30 @@ ARRAY_NEW = solved_12.ARRAY_NEW
 #     }
 # """
 
-ARRAY_DISPOSE = """
+# Example: this is probably the simplest method in the whole library.
+ARRAY_DISPOSE = parse_subroutineDec("""
     /** Disposes this array. */
     method void dispose() {
       do Memory.deAlloc(this);
       return;
     }
-"""
+""")
+
+# Here's where the pieces get put together into a
+ARRAY_CLASS = jack_ast.Class(
+    name="Array",
+    varDecs=ARRAY_CLASS_VARS,
+    subroutineDecs=[
+        ARRAY_NEW,
+        ARRAY_DISPOSE,
+    ])
 
 
-ARRAY_CLASS = f"""
-class Array {{
-{ARRAY_NEW}
-{ARRAY_DISPOSE}
-}}
-"""
-
+#
+# String Library
+#
+# These operations on Strings require a little more thought and considerably more code.
+#
 
 # SOLVERS: if you assume these fields you can implement the methods below one at a time
 STRING_CLASS_VARS = parse_classVarDecs("""
@@ -161,3 +185,138 @@ STRING_CLASS = jack_ast.Class(
         STRING_BACK_SPACE,
         STRING_DOUBLE_QUOTE,
     ])
+
+
+#
+# Memory Library
+#
+# This class is mixed bag. peek() and poke() should be just a couple if lines each,
+# once you figure out how to bend Array references to your will.
+#
+# A simple implementation of alloc() which doesn't re-use freed blocks can be quite
+# simple (and will pass the first test). A more complete implementation can get pretty
+# complex. The second test requires the most simple kind of re-use, and the third test
+# tries to trip up your allocator by repeatedly allocating different-sized blocks. If
+# you pass all three tests, your allocator
+#
+
+# SOLVERS: if you assume these fields you can implement the methods below one at a time
+MEMORY_CLASS_VARS = parse_classVarDecs("""
+    // static Array freePtr;
+""")
+
+MEMORY_INIT = solved_12.MEMORY_INIT
+# MEMORY_INIT = parse_subroutineDec("""
+#     /** Initializes the class. */
+#     function void init() {
+#     }
+# """)
+
+MEMORY_PEEK = solved_12.MEMORY_PEEK
+# MEMORY_PEEK = parse_subroutineDec("""
+#     /** Returns the RAM value at the given address. */
+#     function int peek(int address) {
+#     }
+# """)
+
+MEMORY_POKE = solved_12.MEMORY_POKE
+# MEMORY_POKE = parse_subroutineDec("""
+#     /** Sets the RAM value at the given address to the given value. */
+#     function void poke(int address, int value) {
+#     }
+# """)
+
+MEMORY_ALLOC = solved_12.MEMORY_ALLOC
+# MEMORY_ALLOC = parse_subroutineDec("""
+#     /** Finds an available RAM block of the given size and returns
+#      *  a reference to its base address. */
+#     function int alloc(int size) {
+#     }
+# """)
+
+MEMORY_DE_ALLOC = solved_12.MEMORY_DE_ALLOC
+# MEMORY_DE_ALLOC = parse_subroutineDec("""
+#     /** De-allocates the given object (cast as an array) by making
+#      *  it available for future allocations. */
+#     function void deAlloc(Array o) {
+#     }
+# """)
+
+MEMORY_CLASS = jack_ast.Class(
+    name="Memory",
+    varDecs=MEMORY_CLASS_VARS,
+    subroutineDecs=[
+        MEMORY_INIT,
+        MEMORY_PEEK,
+        MEMORY_POKE,
+        MEMORY_ALLOC,
+        MEMORY_DE_ALLOC,
+    ])
+
+
+#
+# Keyboard Library
+#
+# Welcome to the world of real-time user interaction! Not a lot of code here, but probably
+# some tricky cases to handle, and no real substitue for testing with the UI:
+# ./computer.py .../KeyboardTest/
+#
+
+# TODO
+
+
+#
+# Output Library
+#
+# Use the provided font and this isn't too bad, all things considered. Or stretch
+# yourself and come up with an implementation that doesn't need millions of cycles
+# to initialize itself!
+#
+
+# SOLVERS: if you assume these fields you can implement the methods below one at a time
+OUTPUT_CLASS_VARS = parse_classVarDecs("""
+    // Character map for displaying characters
+    static Array charMaps;
+
+    static int cursorY, cursorX;
+    static String temp;
+""")
+
+OUTPUT_INIT = solved_12.OUTPUT_INIT
+OUTPUT_MOVE_CURSOR = solved_12.OUTPUT_MOVE_CURSOR
+OUTPUT_PRINT_CHAR = solved_12.OUTPUT_PRINT_CHAR
+OUTPUT_PRINT_STRING = solved_12.OUTPUT_PRINT_STRING
+OUTPUT_PRINT_INT = solved_12.OUTPUT_PRINT_INT
+OUTPUT_PRINTLN = solved_12.OUTPUT_PRINTLN
+OUTPUT_BACK_SPACE = solved_12.OUTPUT_BACK_SPACE
+
+OUTPUT_CLASS = solved_12._OUTPUT_CLASS  # HACK
+
+
+#
+# Math Library
+#
+# Take your time, read carefully, and remember to handle the edge cases!
+#
+
+# TODO
+
+
+#
+# Screen Library
+#
+# Lots of details to attend to here, and fairly important to handle some
+# common "easy" cases to get decent speed.
+#
+
+# TODO
+
+
+#
+# Sys Library
+#
+# If you made it this far, the Sys class should be a walk in the park. Consider it your victory
+# lap.
+#
+
+# TODO
--- test_12.py
+++ test_12.py
@@ -2,6 +2,7 @@
 
 from nand import run
 from nand.translate import AssemblySource
+from nand.solutions import solved_12
 import project_05, project_06, project_07, project_08, project_10, project_11
 
 import project_12
@@ -10,7 +11,7 @@ import project_12
 def test_compile_array_lib():
     """First, just make sure this particular class makes it through the compiler."""
 
-    ast = project_10.parse_class(project_12.ARRAY_CLASS)
+    ast = project_12.ARRAY_CLASS
 
     asm = AssemblySource()
     project_11.compile_class(ast, asm)
@@ -18,66 +19,26 @@ def test_compile_array_lib():
     assert len(asm.lines) > 0
 
 
-def test_array_lib(chip=project_05.Computer, assembler=project_06.assemble, translator=project_08.Translator, simulator='codegen'):
-    array_test = project_10.parse_class("""
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/12/ArrayTest/Main.jack
+def test_array_lib(chip=project_05.Computer, assembler=project_06.assemble, translator_class=project_08.Translator, simulator='codegen'):
+    # Note: this one's not so useful interactively, but easier to view anyway
+    array_test = _parse_jack_file("examples/project_12/ArrayTest.jack")
 
-/** Test program for the OS Array class. */
-class Main {
+    translator = translator_class()
 
-    /** Performs several Array manipulations. */
-    function void main() {
-        var Array r;                  // stores test results
-        var Array a, b, c;
+    translator.preamble()
 
-        let r = 8000;
+    _translate_raw_jack(translator, project_12.ARRAY_CLASS)
 
-        let a = Array.new(3);
-        let a[2] = 222;
-        let r[0] = a[2];              // RAM[8000] = 222
+    _translate_dependencies(translator, ["Memory"])
 
-        let b = Array.new(3);
-        let b[1] = a[2] - 100;
-        let r[1] = b[1];              // RAM[8001] = 122
+    _translate_raw_jack(translator, array_test)
 
-        let c = Array.new(500);
-        let c[499] = a[2] - b[1];
-        let r[2] = c[499];            // RAM[8002] = 100
+    translator.finish()
 
-        do a.dispose();
-        do b.dispose();
-
-        let b = Array.new(3);
-        let b[0] = c[499] - 90;
-        let r[3] = b[0];              // RAM[8003] = 10
-
-        do c.dispose();
-        do b.dispose();
-
-        return;
-    }
-}
-""")
-
-    translate = translator()
-
-    translate.preamble()
-
-    _translate_raw_jack(translate, project_10.parse_class(project_12.ARRAY_CLASS))
-
-    _translate_dependencies(translate, ["Memory"])
-
-    _translate_raw_jack(translate, array_test)
-
-    translate.finish()
-
-    check_references(translate)
+    check_references(translator)
 
     computer = run(chip, simulator=simulator)
-    translate.asm.run(assembler, computer, stop_cycles=10_000, debug=True)
+    translator.asm.run(assembler, computer, stop_cycles=10_000, debug=True)
 
     assert computer.peek(8000) == 222
     assert computer.peek(8001) == 122
@@ -94,113 +55,30 @@ def test_compile_string_lib():
     assert len(asm.lines) > 0
 
 
-def test_string_lib(chip=project_05.Computer, assembler=project_06.assemble, translator=project_08.Translator, simulator='codegen'):
-    string_test = project_10.parse_class("""
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/12/StringTest/Main.jack
+def test_string_lib(chip=project_05.Computer, assembler=project_06.assemble, translator_class=project_08.Translator, simulator='codegen'):
+    string_test = _parse_jack_file("examples/project_12/StringTest.jack")
 
-/** Test program for the OS String class. */
-class Main {
+    translator = translator_class()
 
-    /** Performs various string manipulations and displays their results. */
-    function void main() {
-        var String s;
-        var String i;
+    translator.preamble()
 
-        let s = String.new(0); // a zero-capacity string should be supported
-        do s.dispose();
+    _translate_raw_jack(translator, project_12.STRING_CLASS)
 
-        let s = String.new(6); // capacity 6, make sure that length 5 is displayed
-        let s = s.appendChar(97);
-        let s = s.appendChar(98);
-        let s = s.appendChar(99);
-        let s = s.appendChar(100);
-        let s = s.appendChar(101);
-        do Output.printString("new,appendChar: ");
-        do Output.printString(s);                // new, appendChar: abcde
-        do Output.println();
+    _translate_raw_jack(translator, MINIMAL_OUTPUT_LIB)
 
-        let i = String.new(6);
-        do i.setInt(12345);
-        do Output.printString("setInt: ");
-        do Output.printString(i);                // setInt: 12345
-        do Output.println();
+    _translate_dependencies(translator, ["Array", "Memory", "Math"])
 
-        do i.setInt(-32767);
-        do Output.printString("setInt: ");
-        do Output.printString(i);                // setInt: -32767
-        do Output.println();
+    _translate_raw_jack(translator, string_test)
 
-        do Output.printString("length: ");
-        do Output.printInt(s.length());          // length: 5
-        do Output.println();
+    translator.finish()
 
-        do Output.printString("charAt[2]: ");
-        do Output.printInt(s.charAt(2));         // charAt[2]: 99
-        do Output.println();
-
-        do s.setCharAt(2, 45);
-        do Output.printString("setCharAt(2,'-'): ");
-        do Output.printString(s);                // setCharAt(2,'-'): ab-de
-        do Output.println();
-
-        do s.eraseLastChar();
-        do Output.printString("eraseLastChar: ");
-        do Output.printString(s);                // eraseLastChar: ab-d
-        do Output.println();
-
-        let s = "456";
-        do Output.printString("intValue: ");
-        do Output.printInt(s.intValue());        // intValue: 456
-        do Output.println();
-
-        let s = "-32123";
-        do Output.printString("intValue: ");
-        do Output.printInt(s.intValue());        // intValue: -32123
-        do Output.println();
-
-        do Output.printString("backSpace: ");
-        do Output.printInt(String.backSpace());  // backSpace: 129
-        do Output.println();
-
-        do Output.printString("doubleQuote: ");
-        do Output.printInt(String.doubleQuote());// doubleQuote: 34
-        do Output.println();
-
-        do Output.printString("newLine: ");
-        do Output.printInt(String.newLine());    // newLine: 128
-        do Output.println();
-
-        do i.dispose();
-        do s.dispose();
-
-        return;
-    }
-}
-""")
-
-    translate = translator()
-
-    translate.preamble()
-
-    _translate_raw_jack(translate, project_12.STRING_CLASS)
-
-    _translate_raw_jack(translate, MINIMAL_OUTPUT_LIB)
-
-    _translate_dependencies(translate, ["Array", "Memory", "Math"])
-
-    _translate_raw_jack(translate, string_test)
-
-    translate.finish()
-
-    check_references(translate)
+    check_references(translator)
 
     computer = run(chip, simulator=simulator)
 
     output_stream = StringWriter()
-    translate.asm.run(assembler, computer, stop_cycles=400_000, tty=output_stream)
+    # translator.asm.run(assembler, computer, stop_cycles=400_000, tty=output_stream, debug=True)
+    translator.asm.trace(assembler, computer, stop_cycles=500_000, tty=output_stream)
 
     output_lines = "".join(output_stream.strs).split("\n")
     assert output_lines == [
@@ -216,7 +94,121 @@ class Main {
         "backSpace: 129",
         "doubleQuote: 34",
         "newLine: 128",
+        ""
     ]
+
+
+def test_compile_memory_lib():
+    """First, just make sure this particular class makes it through the compiler."""
+
+    ast = project_12.MEMORY_CLASS
+
+    asm = AssemblySource()
+    project_11.compile_class(ast, asm)
+
+    assert len(asm.lines) > 0
+
+def test_memory_lib(chip=project_05.Computer, assembler=project_06.assemble, translator_class=project_08.Translator, simulator='codegen'):
+    # Note: this one's not so useful interactively, but easier to view anyway
+    memory_test = _parse_jack_file("examples/project_12/MemoryTest.jack")
+
+    translator = translator_class()
+
+    translator.preamble()
+
+    _translate_raw_jack(translator, project_12.MEMORY_CLASS)
+
+    # Dependencies; note: need Sys.init to intialize Memory, but don't want the built-in implementation.
+    _translate_raw_jack(translator, solved_12._ARRAY_CLASS)
+    _translate_raw_jack(translator, minimal_sys_lib("Memory"))
+
+    _translate_raw_jack(translator, memory_test)
+
+    translator.finish()
+
+    check_references(translator)
+
+    computer = run(chip, simulator=simulator)
+
+    # translator.asm.run(assembler, computer, stop_cycles=10_000, debug=True)
+    translator.asm.trace(assembler, computer, stop_cycles=10_000)
+
+    # print_free_list(computer)
+
+    assert computer.peek(8000) == 333
+    assert computer.peek(8001) == 334
+    assert computer.peek(8002) == 222
+    assert computer.peek(8003) == 122
+    assert computer.peek(8004) == 100
+    assert computer.peek(8005) == 10
+
+
+def test_memory_lib_reuse():
+    # TODO: a series of alloc and deAlloc calls, enough to require some amount of non-trivial
+    # re-use.
+    pass
+
+
+def test_memory_lib_stress():
+    # TODO: a series of alloc and deAlloc calls for blocks of various sizes, which will
+    # fragment the heap if no effrot is made to prevent it.
+    pass  # TODO
+
+
+def test_compile_output_lib():
+    """First, just make sure this particular class makes it through the compiler."""
+
+    ast = project_12.OUTPUT_CLASS
+
+    asm = AssemblySource()
+    project_11.compile_class(ast, asm)
+
+    assert len(asm.lines) > 0
+
+
+def test_output_lib_debug(chip=project_05.Computer, assembler=project_06.assemble, translator_class=project_08.Translator, simulator='codegen'):
+    memory_test = _parse_jack_file("examples/project_12/OutputTest.jack")
+
+    translator = translator_class()
+
+    translator.preamble()
+
+    _translate_raw_jack(translator, project_12.OUTPUT_CLASS)
+
+    # Dependencies; note: need Sys.init to intialize Output, but don't want the built-in implementation.
+    _translate_raw_jack(translator, solved_12._ARRAY_CLASS)
+    _translate_raw_jack(translator, solved_12._MEMORY_CLASS)
+    _translate_raw_jack(translator, solved_12._STRING_CLASS)
+    _translate_raw_vm(translator, f"nand2tetris/tools/OS/Math.vm")
+    _translate_raw_jack(translator, minimal_sys_lib(["Memory", "Math", "Output"]))
+
+    _translate_raw_jack(translator, memory_test)
+
+    translator.finish()
+
+    check_references(translator)
+
+    computer = run(chip, simulator=simulator)
+
+    # translator.asm.run(assembler, computer, stop_cycles=1_000_000, debug=True)
+    translator.asm.trace(assembler, computer, stop_cycles=10_000_000)
+
+    # TODO: spot check the screen RAM
+    assert computer.peek_screen(0*32 + 0) == 1
+
+
+
+#
+# Helpers, odd and ends, and so forth:
+#
+
+def print_free_list(computer):
+    """Print a summary of the free list, assuming it's laid out the way the included solutions do it."""
+    print("free list:")
+    blockPtr = computer.peek(2048)
+    while blockPtr != 0:
+        print(f"  @{blockPtr}; size: {computer.peek(blockPtr)}")
+        blockPtr = computer.peek(blockPtr+1)
 
 
 class StringWriter:
@@ -226,6 +218,7 @@ class StringWriter:
 
     def write(self, chars):
         self.strs.append(chars)
+        print(chars)
 
 
 ALL_LIBS = ["Memory", "Math", "Screen", "Output", "Keyboard", "Array", "String"]
@@ -235,6 +228,7 @@ def _translate_dependencies(translator, libs):
     _translate_raw_jack(translator, minimal_sys_lib(libs))
     for lib in libs:
         # TEMP: until solved_12 has real implementations
+        print(f"Warning: translating original impl: {lib}")
         _translate_raw_vm(translator, f"nand2tetris/tools/OS/{lib}.vm")
 
 
@@ -262,6 +256,7 @@ class Sys {
     }
 
     function void error(int errorCode) {
+        // would be nice to write the code, but don't want o depend on the Output library.
         // var Array tty;
         // let tty = 24576; // 0x6000
         // let tty[0] = ...;
@@ -271,78 +266,14 @@ class Sys {
 }
 """)
 
-# For test purposes, this implementation of the Output class just writes characters to the "TTY"
-# port, and doesn't update the screen at all. That saves a lot of time, and it means we can test
-# String operations without trying to inspect the screen buffer (or even having drawing to the
-# screen working at all.)
-MINIMAL_OUTPUT_LIB = project_10.parse_class("""
-class Output {
-    field String temp;
+def _parse_jack_file(path):
+    with open(path) as f:
+        src = "\n".join(f.readlines())
+        return project_10.parse_class(src)
 
-    function void init() {
-        let temp = String.new(6);  // enough space for any int
-        return;
-    }
 
-    // Ignored.
-    function void moveCursor(int i, int j) {
-        return;
-    }
+MINIMAL_OUTPUT_LIB = _parse_jack_file("nand/solutions/solved_12/TerminalOutput.jack")
 
-    function void printChar(char c) {
-        var Array tty;
-
-        let tty = 24576;  // 0x6000
-        let tty[0] = c;
-
-        return;
-    }
-
-    function void printString(String s) {
-        // Note: writing directly here (as opposed to calling printChar) saves some cycles.
-        // However, we don't this to be *too* efficient, because whoever's polling the TTY
-        // port needs to check it often enough to see each character as it goes by.
-        // That is, you can't just write one value after another to memory as fast as you
-        // please, and there's no way from inside the CPU to see if the last value you wrote
-        // has been consumed yet.
-        // That's probably not going to be a problem, since String.charAt() should involve
-        // plenty of overhead.
-
-        var Array tty;
-        var int length, ptr;
-
-        let tty = 24576;  // 0x6000
-
-        let length = s.length();
-        let ptr = 0;
-        while (ptr < length) {
-            let tty[0] = s.charAt(ptr);
-            let ptr = ptr + 1;
-        }
-        return;
-    }
-
-    function void printInt(int i) {
-        do temp.setInt(i);
-        do Output.printString(temp);
-        return;
-    }
-
-    function void println() {
-        var Array tty;
-
-        let tty = 24576;   // 0x6000
-        let tty[0] = 128;  // same as String.newLine()
-
-        return;
-    }
-
-    // Ignored, for now. What should it do? Write a backspace character?
-    function void backSpace() {
-        return;
-    }
-}
-""")
 
 def _translate_raw_jack(translator, ast):
     asm = AssemblySource()
