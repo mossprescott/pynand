098cb5804bb39ff5f681f96eaa4b70c3c55a7947
Moss Prescott <moss@theprescotts.com>
2024-03-27 11:24:41 -0400

    Use Ribbit’s own repl-min.scm

--- alt/scheme/repl.py
+++ alt/scheme/repl.py
@@ -2,8 +2,11 @@
 
 """Scheme REPL, reading input from the keyboard and writing output to the screen.
 
-Supported keywords and functions:
-TBD
+This wrapper just runs the equivalent of
+
+    alt/scheme/rvm.py alt/scheme/ribbit/*.scm --simulator compiled --print
+
+Supported keywords and functions: see ribbit/repl-min.scm
 """
 
 from alt.scheme import rvm
@@ -16,23 +19,12 @@ def main():
 
     min_library_src_lines = load("ribbit/min.scm")
     io_src_lines = load("io.scm")
+    repl_src_lines = load("ribbit/repl-min.scm")
 
-    program = "".join(min_library_src_lines + io_src_lines) + """
-
-(repl)
-
-;; Exported symbols.
-
-(export
-  +
-  -
-  *
-  quotient
-  <
-  =
-  cons
-  )
-"""
+    program = "".join(
+        min_library_src_lines
+        + io_src_lines
+        + repl_src_lines)
 
     # Note: actually running the compiler in the Ribbit Python interpreter is pretty slow.
     # Probably want to cache the encoded result somewhere (or just go back to hard-coding it here.)
@@ -41,7 +33,6 @@ def main():
     rvm.run(program, interpreter="jack", simulator="compiled",
             print_asm=True,
             trace_level=rvm.TRACE_NONE)
-            # trace_level=rvm.TRACE_COARSE)
 
 
 if __name__ == "__main__":
