9e48d31d47789ffc18ab8e56da1b70ad722b218a
Moss Prescott <moss@theprescotts.com>
2024-04-24 11:26:57 -0400

    Comments

--- alt/scheme/example/game.scm
+++ alt/scheme/example/game.scm
@@ -4,7 +4,7 @@
 ;; And it consumes memory just waiting for a key to be pressed, because
 ;; it doesn't use the optimized, but terminal-oriented getchar primitive.
 ;;
-;; Requires min.scm and io.scm
+;; Requires ribbit/min.scm and io.scm
 
 (define x 39)
 (define y 12)
@@ -12,19 +12,19 @@
 (define (game)
     (let ((c (peek 4095)))
         (cond
-            ((= c 130)
+            ((= c 130)  ;; left arrow
                 (drawchar x y 0)
                 (set! x (- x 1))
                 (drawchar x y 42))
-            ((= c 131)
+            ((= c 131)  ;; up arrow
                 (drawchar x y 0)
                 (set! y (- y 1))
                 (drawchar x y 42))
-            ((= c 132)
+            ((= c 132)  ;; right arrow
                 (drawchar x y 0)
                 (set! x (+ x 1))
                 (drawchar x y 42))
-            ((= c 133)
+            ((= c 133)  ;; down arrow
                 (drawchar x y 0)
                 (set! y (+ y 1))
                 (drawchar x y 42))
