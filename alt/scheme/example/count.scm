450e1e18171593208ac0d21584d13cd2fc148936
Moss Prescott <moss@theprescotts.com>
2024-05-02 22:59:11 -0400

    Write more numbers per line, now that it will run long enough

--- alt/scheme/example/count.scm
+++ alt/scheme/example/count.scm
@@ -3,12 +3,31 @@
 ;; This program will run indefinitely, continuously allocating ribs but never filling up the heap,
 ;; if garbage-collection is working.
 ;;
-;; Requires min.scm (for write and newline).
+;; Requires min.scm (for write, newline, etc.)
+
+(define (sep) (write-chars (string->list ", ")))
 
 (define (count x)
     (write x)
+    (sep)
+    (write (+ x 1))
+    (sep)
+    (write (+ x 2))
+    (sep)
+    (write (+ x 3))
+    (sep)
+    (write (+ x 4))
+    (sep)
+    (write (+ x 5))
+    (sep)
+    (write (+ x 6))
+    (sep)
+    (write (+ x 7))
+    (sep)
+    (write (+ x 8))
+    (sep)
+    (write (+ x 9))
     (newline)
-    (newline)
-    (count (+ x 1)))
+    (count (+ x 10)))
 
 (count 0)
